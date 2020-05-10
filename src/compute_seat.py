c12f517c0a13060ad1a47ce9a8499a1287206b7b
Darius Irani <dirani2@jhu.edu>
2020-05-09 22:05:38 -0400

    removed padding and attendtion mask

--- src/compute_seat.py
+++ src/compute_seat.py
@@ -30,8 +30,8 @@ class BERTEmbedding(Embedding):
             sentence, 
             add_special_tokens = True,    # Add '[CLS]' and '[SEP]'
             max_length = max_len,         # Pad & truncate all sentences.
-            pad_to_max_length = True,
-            return_attention_mask = True, # Construct attn. masks.
+            pad_to_max_length = False,
+            return_attention_mask = False, # Construct attn. masks.
             return_tensors = 'pt',        # Return pytorch tensors.
         )
         
